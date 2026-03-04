456a02f19fe98d146a1072a49af82de280184704
Akari <arvin.lum@hotmail.com>
2026-03-05 05:45:35 +0800

    cleaned up some file structure redundancy

--- lib/features/library/domain/models/library_model.dart
+++
@@ -1,3 +0,0 @@
-// Automated Image Paths, Data Cleaning & Formatting, The "Cross-Reference" Logic goes here
-// if you find yourself writing too much "IF/ELSE" logic inside library_screen.dart (like determining if a card is a Weapon or Armor to show a specific icon)
-//we will move that logic into library_model.dart. For now, keeping it as a placeholder is perfectly fine for sync purposes.
